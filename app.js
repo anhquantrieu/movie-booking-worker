5cb9c9ea2621624f434ee32402f05d3549c3566e
anhquantrieu <anhquantrieu.01@gmail.com>
2025-05-20 11:11:33 +0700

    api get

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-
+app.get("/", (req, res) => {
+  res.send("Welcome to movie-booking-worker!");
+});
 app.listen(PORT, () => {
   console.log(`Server chạy trên port ${PORT}`);
   console.log("Worker đã được khởi động và lắng nghe job queue.");
